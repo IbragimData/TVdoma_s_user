ae6bebb89d820d9b6eab8c42728efae994122284
argen muratov <ibragim@MacBook-Pro-argen.local>
2024-10-16 12:45:24 +0300

    feat: Заделал сервис для получения пользователя по Id или Mail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,4 +3,17 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+    constructor(
+        private readonly prismaService:PrismaService
+    ){}
+    async getUserByIdOrMail(idOrMail:string){
+        return await  this.prismaService.user.findFirst({
+            where: {
+                OR:[
+                    {mail: idOrMail},
+                    {id: idOrMail}
+                ]
+            }
+        })
+    }
 }
