e8fcfcf032b0dbe98f708705a31ac224e27b08e0
argen muratov <ibragim@MacBook-Pro-argen.local>
2024-10-16 13:44:33 +0300

    feat: Реализовал контролер для создания user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { createUserDto } from './dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService:UserService
+    ){}
+    @Post("")
+    createUser(@Body() dto:createUserDto){
+        return this.userService.createUser(dto)
+    }
+}
