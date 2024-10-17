6cf127cbf531a90be4512fbb8303237aee3a6921
argen muratov <ibragim@MacBook-Pro-argen.local>
2024-10-17 15:55:10 +0300

    feat: Обернул все в try/catch

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { createUserDto, updateUserDto } from './dto';
 import { hashSync } from 'bcrypt';
@@ -9,7 +9,8 @@ export class UserService {
         private readonly prismaService:PrismaService
     ){}
     async getUserByIdOrMail(idOrMail:string){
-        return await  this.prismaService.user.findFirst({
+       try{
+        const user =  await  this.prismaService.user.findFirst({
             where: {
                 OR:[
                     {mail: idOrMail},
@@ -17,36 +18,51 @@ export class UserService {
                 ]
             }
         })
+        return user
+       }catch(e){
+            console.log(e)
+            throw new HttpException("server error user", 500)
+       }
     }
 
     async createUser(dto:createUserDto){
-        const user = await this.getUserByIdOrMail(dto.mail)
-        if(user){
-            throw new BadRequestException()
-        }
-
-        const hashPassword = hashSync(dto.password, 5)
-
-        return await this.prismaService.user.create({
-            data: {
-                mail: dto.mail,
-                password: hashPassword
+        try{
+            const user = await this.getUserByIdOrMail(dto.mail)
+            if(user){
+                throw new BadRequestException()
             }
-        })
+
+            const hashPassword = hashSync(dto.password, 5)
+
+            return await this.prismaService.user.create({
+                data: {
+                    mail: dto.mail,
+                    password: hashPassword
+                }
+            })
+        }catch(e){
+            console.log(e)
+            throw new HttpException("server error user", 500)
+        }
     }
 
     async updateUserName(dto:updateUserDto, userId:string){
-        const user = await this.getUserByIdOrMail(userId)
-        if(!user){
-            throw new BadRequestException()
-        }
-        return await this.prismaService.user.update({
-            where: {
-                id: user.id
-            },
-            data: {
-                ...dto
+        try{
+            const user = await this.getUserByIdOrMail(userId)
+            if(!user){
+                throw new BadRequestException()
             }
-        })
+            return await this.prismaService.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    ...dto
+                }
+            })
+        }catch(e){
+            console.log(e)
+            throw new HttpException("server error user", 500)
+        }
     }
 }
